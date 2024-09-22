4cab21a7bf7d806d9ab0d2ad1734f168dc0ecad8
JUMA KUETH <kuethpathot89@gmail.com>
2024-09-22 14:47:23 +0300

    Add files via upload

---
+++ question4.1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+int main() {
+    int a = 10;
+    int b = 3;
+    char email[20];
+
+    strcpy(email, "juma@vu.com");
+
+    cout << "Email: " << email << endl;
+
+    return 0;
+}
+
+
